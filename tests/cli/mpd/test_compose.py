8fdc06181d5afbd4d6c04e3b0a4742c24b3e5d38
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2024-01-04 20:04:53 +0300

    Move test files of `mpd` commands into separate package

---
+++ tests/cli/mpd/test_compose.py
@@ -0,0 +1,243 @@
+import os
+from pathlib import Path
+from typing import Callable
+from unittest.mock import MagicMock, patch
+from urllib.parse import urljoin
+
+import pytest
+
+import toml
+from freezegun import freeze_time
+
+
+@pytest.mark.parametrize(
+    "audio_formats,video_formats",
+    [
+        ("itag eq 140", "itag eq 243 or itag eq 244"),
+        ("itag eq 140", "none"),
+        ("none", "itag eq 243 or itag eq 244"),
+    ],
+)
+def test_compose_mpd(
+    audio_formats,
+    video_formats,
+    ytpb_cli_invoke: Callable,
+    add_responses_callback_for_reference_base_url: Callable,
+    add_responses_callback_for_segment_urls: Callable,
+    fake_info_fetcher: MagicMock,
+    stream_url: str,
+    tmp_path: Path,
+    expected_out,
+) -> None:
+    add_responses_callback_for_reference_base_url()
+    add_responses_callback_for_segment_urls(
+        r"https://.+\.googlevideo\.com/videoplayback/.+/sq/\w+"
+    )
+    with patch("ytpb.cli.common.YtpbInfoFetcher") as mock_fetcher:
+        mock_fetcher.return_value = fake_info_fetcher
+        result = ytpb_cli_invoke(
+            [
+                "mpd",
+                "compose",
+                "--no-cache",
+                "--interval",
+                "7959120/7959121",
+                "-af",
+                audio_formats,
+                "-vf",
+                video_formats,
+                stream_url,
+            ],
+        )
+    assert result.exit_code == 0
+    assert result.output == expected_out
+
+
+@freeze_time("2023-03-26T00:00:00+00:00")
+def test_compose_mpd_with_no_streams(
+    ytpb_cli_invoke: Callable,
+    fake_info_fetcher: MagicMock,
+    stream_url: str,
+    video_id: str,
+    audio_base_url: str,
+    video_base_url: str,
+    tmp_path: Path,
+    expected_out,
+) -> None:
+    with patch("ytpb.cli.common.YtpbInfoFetcher") as mock_fetcher:
+        mock_fetcher.return_value = fake_info_fetcher
+        result = ytpb_cli_invoke(
+            [
+                "mpd",
+                "compose",
+                "--no-cache",
+                "--interval",
+                "2023-03-25T23:33:55+00:00/2023-03-25T23:33:58+00:00",
+                "-af",
+                "itag eq 0",
+                "-vf",
+                "itag eq 0",
+                stream_url,
+            ],
+        )
+
+    assert result.exit_code == 1
+    assert result.output == expected_out
+
+
+def test_compose_mpd_using_yt_dlp(
+    ytpb_cli_invoke: Callable,
+    add_responses_callback_for_reference_base_url: Callable,
+    add_responses_callback_for_segment_urls: Callable,
+    fake_info_fetcher: MagicMock,
+    stream_url: str,
+    video_id: str,
+    audio_base_url: str,
+    video_base_url: str,
+    tmp_path: Path,
+) -> None:
+    add_responses_callback_for_reference_base_url()
+    add_responses_callback_for_segment_urls(
+        urljoin(audio_base_url, r"sq/\w+"),
+    )
+
+    with patch("ytpb.cli.common.YoutubeDLInfoFetcher") as mock_fetcher:
+        mock_fetcher.return_value = fake_info_fetcher
+        result = ytpb_cli_invoke(
+            [
+                "mpd",
+                "compose",
+                "--yt-dlp",
+                "--no-cache",
+                "--interval",
+                "7959120/7959121",
+                "-af",
+                "itag eq 140",
+                "-vf",
+                "",
+                stream_url,
+            ],
+        )
+
+    assert result.exit_code == 0
+
+
+def test_with_default_config(
+    ytpb_cli_invoke: Callable,
+    add_responses_callback_for_reference_base_url: Callable,
+    add_responses_callback_for_segment_urls: Callable,
+    fake_info_fetcher: MagicMock,
+    stream_url: str,
+    tmp_path: Path,
+) -> None:
+    # Given:
+    add_responses_callback_for_reference_base_url()
+    add_responses_callback_for_segment_urls(
+        r"https://.+\.googlevideo\.com/videoplayback/.+/sq/\w+"
+    )
+
+    config = {
+        "options": {
+            "mpd": {
+                "compose": {
+                    "audio_formats": "NON-SENS",
+                }
+            }
+        }
+    }
+    config_path = Path(os.getenv("XDG_CONFIG_HOME")) / "ytpb/config.toml"
+    config_path.parent.mkdir(parents=True)
+    with config_path.open("w") as f:
+        toml.dump(config, f)
+
+    # When:
+    with patch("ytpb.cli.common.YtpbInfoFetcher") as mock_fetcher:
+        mock_fetcher.return_value = fake_info_fetcher
+        result = ytpb_cli_invoke(
+            [
+                "mpd",
+                "compose",
+                "--no-cache",
+                "--interval",
+                "7959120/7959121",
+                "-af",
+                "itag eq 140",
+                "-vf",
+                "itag eq 243",
+                stream_url,
+            ],
+        )
+
+    # Then:
+    assert result.exit_code == 0
+
+
+def test_compose_to_custom_output_path(
+    ytpb_cli_invoke: Callable,
+    add_responses_callback_for_reference_base_url: Callable,
+    add_responses_callback_for_segment_urls: Callable,
+    fake_info_fetcher: MagicMock,
+    stream_url: str,
+    tmp_path: Path,
+):
+    add_responses_callback_for_reference_base_url()
+    add_responses_callback_for_segment_urls(
+        r"https://.+\.googlevideo\.com/videoplayback/.+/sq/\w+"
+    )
+    with patch("ytpb.cli.common.YtpbInfoFetcher") as mock_fetcher:
+        mock_fetcher.return_value = fake_info_fetcher
+        result = ytpb_cli_invoke(
+            [
+                "mpd",
+                "compose",
+                "--no-cache",
+                "--interval",
+                "7959120/7959121",
+                "-af",
+                "itag eq 140",
+                "-vf",
+                "itag eq 243",
+                "-o",
+                "manifest.xml",
+                stream_url,
+            ],
+            catch_exceptions=False,
+        )
+    assert result.exit_code == 0
+    assert os.path.exists("manifest.xml")
+
+
+def test_compose_to_custom_template_output_path(
+    ytpb_cli_invoke: Callable,
+    add_responses_callback_for_reference_base_url: Callable,
+    add_responses_callback_for_segment_urls: Callable,
+    fake_info_fetcher: MagicMock,
+    stream_url: str,
+    video_id: str,
+    tmp_path: Path,
+):
+    add_responses_callback_for_reference_base_url()
+    add_responses_callback_for_segment_urls(
+        r"https://.+\.googlevideo\.com/videoplayback/.+/sq/\w+"
+    )
+    with patch("ytpb.cli.common.YtpbInfoFetcher") as mock_fetcher:
+        mock_fetcher.return_value = fake_info_fetcher
+        result = ytpb_cli_invoke(
+            [
+                "mpd",
+                "compose",
+                "--no-cache",
+                "--interval",
+                "7959120/7959121",
+                "-af",
+                "itag eq 140",
+                "-vf",
+                "itag eq 243",
+                "-o",
+                "<id>.xml",
+                stream_url,
+            ],
+            catch_exceptions=False,
+        )
+    assert result.exit_code == 0
+    assert os.path.exists(f"{video_id}.xml")
---
+++ tests/cli/mpd/test_refresh.py
@@ -0,0 +1,111 @@
+# ruff: noqa: E501
+
+import copy
+from dataclasses import asdict
+from difflib import unified_diff
+from pathlib import Path
+from typing import Callable
+from unittest.mock import MagicMock, patch
+
+import freezegun
+
+import pytest
+from freezegun import freeze_time
+
+from helpers import patched_freezgun_astimezone
+
+from ytpb.info import YouTubeVideoInfo
+from ytpb.playback import Playback
+from ytpb.streams import Streams
+from ytpb.types import AudioOrVideoStream, AudioStream, VideoStream
+
+freezegun.api.FakeDatetime.astimezone = patched_freezgun_astimezone
+
+
+@pytest.fixture()
+def dash_manifest(audio_base_url: str, video_base_url: str) -> str:
+    return f"""<?xml version='1.0' encoding='UTF-8'?>
+<!--This file is created with ytpb, and expires at 2023-08-16T04:24:48+02:00-->
+<MPD xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="urn:mpeg:DASH:schema:MPD:2011" xsi:schemaLocation="urn:mpeg:DASH:schema:MPD:2011 DASH-MPD.xsd" profiles="urn:mpeg:dash:profile:isoff-main:2011" type="static" mediaPresentationDuration="PT66S">
+  <Period duration="PT66S">
+    <AdaptationSet id="0" mimeType="audio/mp4" subsegmentAlignment="true">
+      <SegmentTemplate media="sq/$Number$" startNumber="7959120" duration="2000" timescale="1000"/>
+      <Representation id="140" codecs="mp4a.40.2" startWithSAP="1" audioSamplingRate="44100">
+        <BaseURL>{audio_base_url}</BaseURL>
+      </Representation>
+    </AdaptationSet>
+    <AdaptationSet id="1" mimeType="video/webm" subsegmentAlignment="true">
+      <SegmentTemplate media="sq/$Number$" startNumber="7959120" duration="2000" timescale="1000"/>
+      <Representation id="244" codecs="vp9" startWithSAP="1" width="854" height="480" maxPlayoutRate="1" frameRate="30">
+        <BaseURL>{video_base_url}</BaseURL>
+      </Representation>
+    </AdaptationSet>
+  </Period>
+</MPD>
+"""
+
+
+@freeze_time("2023-08-16T02:30:00+02:00", tz_offset=2)
+def test_refresh_mpd(
+    ytpb_cli_invoke: Callable,
+    mock_fetch_and_set_essential: MagicMock,
+    streams_in_list: list[AudioOrVideoStream],
+    active_live_video_info: YouTubeVideoInfo,
+    stream_url: str,
+    video_id: str,
+    audio_base_url: str,
+    video_base_url: str,
+    dash_manifest: str,
+    tmp_path: Path,
+    expected_out,
+) -> None:
+    # Given:
+    manifest_path = tmp_path / "manifest.mpd"
+    with open(manifest_path, "w") as f:
+        f.write(dash_manifest)
+
+    # When:
+    def mock_essential(obj: Playback, *args, **kwargs):
+        updated_streams = copy.deepcopy(streams_in_list)
+        for i, stream in enumerate(updated_streams):
+            stream_as_dict = asdict(stream)
+            stream_as_dict["base_url"] = "https://test/expire/1692153000/"
+            if "audio" in stream.mime_type:
+                updated_streams[i] = AudioStream(**stream_as_dict)
+            else:
+                updated_streams[i] = VideoStream(**stream_as_dict)
+            obj._streams = Streams(updated_streams)
+            obj._info = active_live_video_info
+
+    with patch.object(
+        Playback, "fetch_and_set_essential", side_effect=mock_essential, autospec=True
+    ):
+        result = ytpb_cli_invoke(["mpd", "refresh", str(manifest_path)])
+
+    # Then:
+    assert result.exit_code == 0
+    assert result.output == expected_out
+
+    with open(manifest_path) as f:
+        refreshed_manifest = f.read()
+
+    actual_diff = list(
+        unified_diff(
+            dash_manifest.splitlines(keepends=True),
+            refreshed_manifest.splitlines(keepends=True),
+            n=0,
+        )
+    )
+    assert actual_diff == [
+        "--- \n",
+        "+++ \n",
+        "@@ -2 +2 @@\n",
+        "-<!--This file is created with ytpb, and expires at 2023-08-16T04:24:48+02:00-->\n",
+        "+<!--This file is created with ytpb, and expires at 2023-08-16T04:30:00+02:00-->\n",
+        "@@ -8 +8 @@\n",
+        f"-        <BaseURL>{audio_base_url}</BaseURL>\n",
+        "+        <BaseURL>https://test/expire/1692153000/</BaseURL>\n",
+        "@@ -14 +14 @@\n",
+        f"-        <BaseURL>{video_base_url}</BaseURL>\n",
+        "+        <BaseURL>https://test/expire/1692153000/</BaseURL>\n",
+    ]
