250c09fbf2548b19517b360f83f5d02c5425fbb2
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2024-01-07 19:03:48 +0300

    Remove unused OrderedGroup and RewindCommand

--- src/ytpb/cli/custom.py
+++ src/ytpb/cli/custom.py
@@ -3,20 +3,5 @@ import collections
 import click
 
 
-class OrderedGroup(click.Group):
-    def __init__(self, name=None, commands=None, **attrs):
-        super().__init__(name, commands, **attrs)
-        self.commands = commands or collections.OrderedDict()
-
-    def list_commands(self, ctx):
-        return self.commands
-
-
-class RewindCommand(click.Command):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.params.insert(0, click.Option())
-
-
 def get_parameter_by_name(name: str, ctx: click.Context) -> click.Parameter:
     return next((p for p in ctx.command.params if p.name == name), None)
