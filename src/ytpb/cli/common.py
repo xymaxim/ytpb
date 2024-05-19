4b4d62397833854213cc34c5d7a90e1ed78efd48
Maxim Stolyarchuk <maxim.stolyarchuk@gmail.com>
2024-05-19 13:50:38 +0300

    refactor(cli): Change functions order in the module

--- src/ytpb/cli/common.py
+++ src/ytpb/cli/common.py
@@ -29,49 +29,6 @@ CONSOLE_TEXT_WIDTH = 80
 EARLIEST_DATE_TIMEDELTA = timedelta(days=6, hours=23)
 
 
-def get_parameter_by_name(name: str, ctx: click.Context) -> click.Parameter:
-    return next((p for p in ctx.command.params if p.name == name), None)
-
-
-def create_playback(ctx: click.Context) -> Playback:
-    stream_url = ctx.params["stream_url"]
-
-    if ctx.params["yt_dlp"]:
-        fetcher = YoutubeDLInfoFetcher(stream_url)
-    else:
-        fetcher = YtpbInfoFetcher(stream_url)
-
-    try:
-        click.echo(f"Run playback for {stream_url}")
-        click.echo("(<<) Collecting info about the video...")
-
-        force_update_cache = ctx.params.get("force_update_cache", False)
-        need_read_cache = not (ctx.params.get("no_cache", True) or force_update_cache)
-        if need_read_cache:
-            try:
-                playback = Playback.from_cache(stream_url, fetcher=fetcher)
-            except CachedItemNotFoundError:
-                logger.debug("Could not find unexpired cached item for the video")
-                playback = Playback.from_url(
-                    stream_url, fetcher=fetcher, write_to_cache=True
-                )
-        else:
-            playback = Playback.from_url(
-                stream_url, fetcher=fetcher, write_to_cache=force_update_cache
-            )
-    except BroadcastStatusError as e:
-        match e.status:
-            case BroadcastStatus.NONE:
-                click.echo("It's seems that the video is not a live stream", err=True)
-            case BroadcastStatus.COMPLETED:
-                click.echo("Stream was live, but now it's finished", err=True)
-        sys.exit(1)
-
-    click.echo(f"Stream '{playback.info.title}' is alive!")
-
-    return playback
-
-
 def normalize_stream_url(ctx: click.Context, param: click.Argument, value: str) -> str:
     try:
         return normalize_video_url(value)
@@ -85,6 +42,24 @@ stream_argument = cloup.argument(
     help="Stream URL or video ID.",
     callback=normalize_stream_url,
 )
+
+
+def suppress_output() -> None:
+    sys.stdout = open(os.devnull, "w")
+
+
+def echo_notice(message: str) -> None:
+    click.echo(f"~ {message}")
+
+
+def get_parameter_by_name(name: str, ctx: click.Context) -> click.Parameter:
+    return next((p for p in ctx.command.params if p.name == name), None)
+
+
+def resolve_output_path(output_path: Path) -> Path:
+    resolved_path = Path(output_path).expanduser().resolve()
+    resolved_path.parent.mkdir(parents=True, exist_ok=True)
+    return resolved_path
 
 
 def prepare_line_for_summary_info(
@@ -141,6 +116,45 @@ def print_summary_info(
     )
 
 
+def create_playback(ctx: click.Context) -> Playback:
+    stream_url = ctx.params["stream_url"]
+
+    if ctx.params["yt_dlp"]:
+        fetcher = YoutubeDLInfoFetcher(stream_url)
+    else:
+        fetcher = YtpbInfoFetcher(stream_url)
+
+    try:
+        click.echo(f"Run playback for {stream_url}")
+        click.echo("(<<) Collecting info about the video...")
+
+        force_update_cache = ctx.params.get("force_update_cache", False)
+        need_read_cache = not (ctx.params.get("no_cache", True) or force_update_cache)
+        if need_read_cache:
+            try:
+                playback = Playback.from_cache(stream_url, fetcher=fetcher)
+            except CachedItemNotFoundError:
+                logger.debug("Could not find unexpired cached item for the video")
+                playback = Playback.from_url(
+                    stream_url, fetcher=fetcher, write_to_cache=True
+                )
+        else:
+            playback = Playback.from_url(
+                stream_url, fetcher=fetcher, write_to_cache=force_update_cache
+            )
+    except BroadcastStatusError as e:
+        match e.status:
+            case BroadcastStatus.NONE:
+                click.echo("It's seems that the video is not a live stream", err=True)
+            case BroadcastStatus.COMPLETED:
+                click.echo("Stream was live, but now it's finished", err=True)
+        sys.exit(1)
+
+    click.echo(f"Stream '{playback.info.title}' is alive!")
+
+    return playback
+
+
 def query_streams_or_exit(
     streams: SetOfStreams,
     format_spec: str,
@@ -176,6 +190,38 @@ def query_streams_or_exit(
         sys.exit(1)
 
     return queried_streams
+
+
+def raise_for_too_far_sequence(
+    sequence: int,
+    current_sequence: int,
+    base_url: str,
+    ctx: click.Context,
+    param_name: str,
+) -> None:
+    segment_duration = int(float(extract_parameter_from_url("dur", base_url)))
+    earliest_sequence = (
+        current_sequence - EARLIEST_DATE_TIMEDELTA.total_seconds() // segment_duration
+    )
+    if sequence <= earliest_sequence:
+        click.echo("\n")
+        raise click.BadParameter(
+            f"Sequence number is beyond the limit of 7 days: {sequence}",
+            param=get_parameter_by_name(param_name, ctx),
+        )
+
+
+def raise_for_sequence_ahead_of_current(
+    sequence, current_sequence: int, ctx: click.Context, param_name: str
+) -> None:
+    if sequence >= current_sequence:
+        click.echo("\n")
+        message = (
+            "Sequence number is ahead or equal to the current one: "
+            f"{sequence} >= {current_sequence}"
+        )
+        param = get_parameter_by_name(param_name, ctx)
+        raise click.BadParameter(message, ctx, param)
 
 
 def find_earliest_sequence(
@@ -232,49 +278,3 @@ def find_earliest_sequence(
     logger.debug("Found the earliest segment", sequence=result)
 
     return result
-
-
-def raise_for_too_far_sequence(
-    sequence: int,
-    current_sequence: int,
-    base_url: str,
-    ctx: click.Context,
-    param_name: str,
-) -> None:
-    segment_duration = int(float(extract_parameter_from_url("dur", base_url)))
-    earliest_sequence = (
-        current_sequence - EARLIEST_DATE_TIMEDELTA.total_seconds() // segment_duration
-    )
-    if sequence <= earliest_sequence:
-        click.echo("\n")
-        raise click.BadParameter(
-            f"Sequence number is beyond the limit of 7 days: {sequence}",
-            param=get_parameter_by_name(param_name, ctx),
-        )
-
-
-def raise_for_sequence_ahead_of_current(
-    sequence, current_sequence: int, ctx: click.Context, param_name: str
-) -> None:
-    if sequence >= current_sequence:
-        click.echo("\n")
-        message = (
-            "Sequence number is ahead or equal to the current one: "
-            f"{sequence} >= {current_sequence}"
-        )
-        param = get_parameter_by_name(param_name, ctx)
-        raise click.BadParameter(message, ctx, param)
-
-
-def resolve_output_path(output_path: Path) -> Path:
-    resolved_path = Path(output_path).expanduser().resolve()
-    resolved_path.parent.mkdir(parents=True, exist_ok=True)
-    return resolved_path
-
-
-def suppress_output() -> None:
-    sys.stdout = open(os.devnull, "w")
-
-
-def echo_notice(message: str) -> None:
-    click.echo(f"~ {message}")
